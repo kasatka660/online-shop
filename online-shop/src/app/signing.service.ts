cc89c703b024136511704018f09fccef0e921b71
Alena Khaliauskaya <KhaliauskayaA@iba>
2019-04-02 15:13:00 +0300

    fix: fix sign in mechanism

--- online-shop/src/app/signing.service.ts
+++ online-shop/src/app/signing.service.ts
@@ -23,25 +23,18 @@ export class SigningService {
   checkUser(userToSearch: User) {
     return this.http.get<User[]>(this.usersUrl)
       .pipe(
-        map( item => console.log(item))
+        map( item => {
+          const userFound = item.find( user => user.email === userToSearch.email);
+          //console.log(userFound)
+          if ( typeof userFound == 'undefined' ) {
+           // console.log(typeof userFound);
+            return false
+          } else {
+           // console.log(userFound.password === userToSearch.password)
+            return userFound.password === userToSearch.password;
+          }
+        })
       )
   }
 }
 
-
-/*
-checkUser(userToSearch: User) {
-    return this.http.get<User[]>(this.usersUrl)
-      .pipe( result => {
-        this.allUsers = result;
-        let userFound =  this.allUsers.find( user => user.email === userToSearch.email );
-        if ( typeof userFound == 'undefined' ) {
-          return false
-        } else {
-          return userFound.password === userToSearch.password;
-        }
-      });
-  }
-
-
- */
